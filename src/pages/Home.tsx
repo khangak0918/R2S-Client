57aa5a0a56f20aa08e0ab705249aa09e723ef21f
Demo <khangak0918@gmail.com>
2025-02-28 22:37:41 +0700

    [khang] Test1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import { ReactElement } from 'react'
-import Table from '../components/table /Table'
 
 function Home(): ReactElement {
   return (
     <div className="">
       <div>This is home page</div>      
       <div className="overflow-x-auto rounded-lg shadow">
-        <Table />
+
       </div>
     </div>
   )
